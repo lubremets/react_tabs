249d0039a55c81ace3e0af79db1c01b7b90ea860
lubremets <lubremets@gmail.com>
2019-11-08 10:31:33 +0200

    Updates

---
+++ precache-manifest.73e147af7e536cd4497e987736795d1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "517ba2bf2e3a7198fcab697dfcbb1099",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "325b66de52c8eceee24b",
+    "url": "/react_tabs/static/css/main.3eece42e.chunk.css"
+  },
+  {
+    "revision": "a16792bbd021bec6ef7e",
+    "url": "/react_tabs/static/js/2.27899654.chunk.js"
+  },
+  {
+    "revision": "325b66de52c8eceee24b",
+    "url": "/react_tabs/static/js/main.be06e535.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
